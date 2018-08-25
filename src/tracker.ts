c895391202174e8c87c10c26b98c3800157e5702
Vladislav Semenov <vladislav.semenov@gmail.com>
2018-08-25 16:54:15 +0300

    Added tracker

---
+++ src/tracker.ts
@@ -0,0 +1,30 @@
+import * as Hapi from 'hapi';
+
+async function startTracker() {
+  const server = new Hapi.Server({
+    host: 'localhost',
+    port: 6000,
+  });
+
+  let nodes = {};
+
+  server.route({
+    method: 'GET',
+    path: '/nodes',
+    handler: (request: Hapi.Request) => {
+      return nodes;
+    },
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/nodes',
+    handler: async (request: Hapi.Request) => {
+      nodes = request.payload;
+    },
+  });
+
+  await server.start();
+}
+
+startTracker();
